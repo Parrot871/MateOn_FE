20588f8994a14a7178fe5db462e14a0ededfa359
circular <taehyeonee823@dankook.ac.kr>
2026-07-12 20:05:40 +0900

    카카오 로그인 연동

---
+++ app.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  expo: {
+    name: "MateOn_FE",
+    slug: "MateOn_FE",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/logo_light.png",
+    scheme: "mateonfe",
+    userInterfaceStyle: "automatic",
+    ios: {
+      icon: "./assets/images/logo_light.png",
+      bundleIdentifier: "com.anonymous.MateOn-FE",
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/logo_light.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          backgroundColor: "#8BA9FF",
+          image: "./assets/images/logo_light.png",
+          imageWidth: 120,
+        },
+      ],
+      [
+        "@react-native-seoul/kakao-login",
+        {
+          kakaoAppKey: process.env.KAKAO_NATIVE_APP_KEY,
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    android: {
+      package: "com.anonymous.MateOn_FE",
+    },
+  },
+};
