50c6df026b245c4c8e3783595ca91efc90d390f0
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-07-12 11:12:46 +0900

    chore: expo-dev-client 추가 및 스플래시 화면 커스텀, 기본 라우트 구성

---
+++ src/app/(auth)/signup.tsx
@@ -0,0 +1,10 @@
+// src/app/(auth)/login.tsx
+import { Text, View } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>로그인 화면</Text>
+    </View>
+  );
+}
